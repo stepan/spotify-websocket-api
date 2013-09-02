894d6a6c78dd41e2942cd5b8e7918b13fe8f07cc
Stepan Grigoryan <stepang@gmail.com>
2013-09-02 16:36:18 -0700

    Removed unnecessary fiels

--- install-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e -x
-VER=$TRAVIS_PYTHON_VERSION
-
-apt-get update -yqq || true
-apt-get install -yqq python$VER
-pip install -q requests protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
